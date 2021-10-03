b809bb11a9023eea1eaf4c92c545d6bc70cfa88c
Kyle Kotowick <kotowick@invictonlabs.com>
2021-10-03 10:22:00 -0400

    Rename "depends" variable and add "consider_first_use_as_changed" variable

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 // Get the existing state
 module "state" {
   source  = "Invicton-Labs/get-state/null"
-  version = "0.2.1"
+  version = "~> 0.2.1"
 }
 
 locals {
@@ -17,7 +17,7 @@ resource "null_resource" "keeper" {
     "___CONDITIONAL_TRIGGER_module_id" = random_uuid.module_id.id
     trigger                            = local.trigger_json
     // This is a way to force the re-create to wait until other (optional) resources or data sources have finished
-    depends = var.depends == null ? "" : ""
+    save_depends_on = var.save_depends_on == null ? "" : ""
   }
   lifecycle {
     create_before_destroy = true
@@ -30,6 +30,7 @@ locals {
     resource
     if resource.type == "null_resource" && resource.name == "keeper" ? lookup(resource.instances[0].attributes.triggers, "___CONDITIONAL_TRIGGER_module_id", null) == random_uuid.module_id.id : false
   ]
-  existing_trigger_json = length(local.existing_resources) > 0 ? local.existing_resources[0].instances[0].attributes.triggers.trigger : null
-  triggers_modified     = local.existing_trigger_json != local.trigger_json
+  no_existing_state     = length(local.existing_resources) == 0
+  existing_trigger_json = local.no_existing_state ? null : local.existing_resources[0].instances[0].attributes.triggers.trigger
+  triggers_modified     = local.no_existing_state ? var.consider_first_use_as_changed : local.existing_trigger_json != local.trigger_json
 }
--- variables.tf
+++ variables.tf
@@ -3,7 +3,13 @@ variable "value" {
   type        = any
 }
 
-variable "depends" {
+variable "consider_first_use_as_changed" {
+  description = "Whether to return `true` for the `changed` output if this module is being used for the first time (no existing value in state)."
+  type        = bool
+  default     = true
+}
+
+variable "save_depends_on" {
   description = "An optional list of things to wait for before storing the new value in the state. Use this to ensure things complete successfully before storing the new value, so that if they fail then on re-apply this module will still show that the value has changed."
   type        = list(any)
   default     = []
